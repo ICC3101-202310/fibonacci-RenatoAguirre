01392e56bbe7d32feaa353458187a35b50db4a41
Rena <raaguirre1@miuandes.cl>
2023-09-19 15:55:07 -0300

    agregada la funcion fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+function fibonacci(n){
+    if (n == 0){
+        return 0;
+    }
+    else if (n == 1){
+        return 1;
+    }
+    else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(15));
+console.log(fibonacci(5));
+console.log(fibonacci(16));
+console.log(fibonacci(2));
+
